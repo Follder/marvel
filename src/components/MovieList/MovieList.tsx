95198cf1494e67c4b2ebb68cddde4e9f83d9eb1c
Dmytro Koriahin <DKoryahin@gmail.com>
2024-07-13 00:16:20 +0300

    implement movieList

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,25 +1,47 @@
 import React, { Component } from "react";
 import "./MovieList.scss";
 import { CharacterComics } from "../../types/Character";
+import { Button } from "../Button/Button";
 
 interface Props {
   comics: CharacterComics[];
 }
 
-// interface state {
-//   isLongComicsList: boolean;
-// }
+interface State {
+  isLongComicsList: boolean;
+}
 
-export class MovieList extends Component<Props> {
-  state = {
-    isLongComicsList: false,
+export class MovieList extends Component<Props, State> {
+  MAX_DISPLAYED_COMICS: number;
+  
+  constructor(props: Props) {
+    super(props);
+    this.MAX_DISPLAYED_COMICS = 10;
+    this.state = {
+      isLongComicsList: props.comics.length <= this.MAX_DISPLAYED_COMICS
+    }
+  }
+
+  componentDidUpdate(prevProps: Props): void {
+    if(prevProps.comics !== this.props.comics) {
+      this.setState({isLongComicsList: this.props.comics.length <= this.MAX_DISPLAYED_COMICS})
+    }
+  }
+
+  makeShortMovieList = (comics: CharacterComics[]) => {
+    if (comics.length > this.MAX_DISPLAYED_COMICS) {
+      return comics.slice(0, this.MAX_DISPLAYED_COMICS);
+    }
+
+    return comics;
   }
 
   render() {
-    const { comics } = this.props;
+    const {comics} = this.props;
+    const shortComics = this.makeShortMovieList(this.props.comics);
 
-      let shortComicList = comics;
-   shortComicList.length = 15;
+
+    console.log(comics, this.state.isLongComicsList);
 
     return (
       <div className="movie-list">
@@ -27,21 +49,21 @@ export class MovieList extends Component<Props> {
   
         {this.props.comics.length > 0 ? (
         <ul className="movie-list__movies">
-          {this.state.isLongComicsList ? (
-            <>
-              {shortComicList.map((item, i) => (
-                <li className="movie-list__movie" key={i}>
-                  {item.name}
-                </li>
-              ))}
-              <div onClick={() => this.setState({isLongComicsList: true})}>load more</div>
-            </>
-          ) : (
+          {this.state.isLongComicsList && comics.length >= this.MAX_DISPLAYED_COMICS ? (
             comics.map((item, i) => (
               <li className="movie-list__movie" key={i}>
                 {item.name}
               </li>
             ))
+          ) : (
+            <>
+            {shortComics.map((item, i) => (
+              <li className="movie-list__movie" key={i}>
+                {item.name}
+              </li>
+            ))}
+            {comics.length > this.MAX_DISPLAYED_COMICS && <div className="movie-list__button" onClick={() => this.setState({isLongComicsList: true})}><Button text="load more" /></div>}
+          </>
           )}
         </ul>
       ) : (
